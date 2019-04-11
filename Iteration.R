b6e7d69a87524138855a1de6ed9988f2fb5575b4
Prathmesh Jirange <prathmesh.jirange@onsemi.com>
2019-04-11 16:08:58 -0700

    completed iteration

--- Iteration.R
+++ Iteration.R
@@ -209,3 +209,102 @@ is_ok = y$error %>% map_lgl(is_null)
 x[!is_ok]
 
 y$result[is_ok] %>% flatten_dbl()
+
+# multiple arguements in map2
+
+mu = c(1,10,5)
+sigma = c(1,5,3)
+
+map_2_f = map2(mu,sigma,rnorm,n =5)
+
+# more than 2 arguements
+n <- list(1, 3, 5)
+args1 <- list(n, mu, sigma)
+args1 %>%
+  pmap(rnorm) %>% 
+  str()
+
+args2 <- list(mean = mu, sd = sigma, n = n)
+args2 %>% 
+  pmap(rnorm) %>% 
+  str()
+
+params <- tribble(
+  ~mean, ~sd, ~n,
+  5,     1,  1,
+  10,     5,  3,
+  -3,    10,  5
+)
+params %>% 
+  pmap(rnorm)
+
+# invoke map function
+
+f <- c("runif", "rnorm", "rpois")
+param <- list(
+  list(min = -1, max = 1), 
+  list(sd = 5), 
+  list(lambda = 10)
+)
+
+invoke_map(f, param, n = 5) %>% str()
+
+# using tribble to make it easy
+
+sim <- tribble(
+  ~f,      ~params,
+  "runif", list(min = -1, max = 1),
+  "rnorm", list(sd = 5),
+  "rpois", list(lambda = 10)
+)
+
+sim %>% 
+  mutate(sim = invoke_map(f, params, n = 10))
+
+# use of walk function to store the output
+
+library(ggplot2)
+plots <- mtcars %>% 
+  split(.$cyl) %>% 
+  map(~ggplot(., aes(mpg, wt)) + geom_point())
+paths <- stringr::str_c(names(plots), ".pdf")
+
+
+pwalk(list(paths, plots), ggsave, path = tempdir())
+
+# misc functions
+ # some and every function
+
+x <- list(1:5, letters, list(10))
+
+x %>% 
+  some(is_character)
+
+  #detect & detect_index function
+x <- sample(10)
+x
+
+x %>% 
+  detect(~ . > 5)
+
+x %>% 
+  detect_index(~ . > 5)
+
+
+# reduce and accumulate
+
+dfs <- list(
+  age = tibble(name = "John", age = 30),
+  sex = tibble(name = c("John", "Mary"), sex = c("M", "F")),
+  trt = tibble(name = "Mary", treatment = "A")
+)
+
+dfs %>% reduce(full_join)
+
+vs <- list(
+  c(1, 3, 5, 6, 10),
+  c(1, 2, 3, 7, 8, 10),
+  c(1, 2, 3, 4, 8, 9, 10)
+)
+
+vs %>% reduce(intersect)
