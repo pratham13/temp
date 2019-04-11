df4ff7b85b1de16cd472757cf05b322f137048ff
Prathmesh Jirange <prathmesh.jirange@onsemi.com>
2019-04-11 15:04:38 -0700

    addtion of safely

--- Iteration.R
+++ Iteration.R
@@ -138,3 +138,74 @@ col_summary <- function(df, fun) {
 
 
 col_summary(df,mean)
+
+# using map functions of purrr
+
+models <- mtcars %>% 
+  split(.$cyl) %>% 
+  map(~lm(mpg ~ wt, data = .))
+
+models %>%
+  map(summary) %>% map_dbl("adj.r.squared")
+
+
+#comparison of apply and map functions
+
+x1 <- list(
+  c(0.27, 0.37, 0.57, 0.91, 0.20),
+  c(0.90, 0.94, 0.66, 0.63, 0.06), 
+  c(0.21, 0.18, 0.69, 0.38, 0.77)
+)
+x2 <- list(
+  c(0.50, 0.72, 0.99, 0.38, 0.78), 
+  c(0.93, 0.21, 0.65, 0.13, 0.27), 
+  c(0.39, 0.01, 0.38, 0.87, 0.34)
+)
+
+threshold <- function(x, cutoff = 0.8) x[x > cutoff]
+
+x1 %>% sapply(threshold) %>% str()
+x2 %>% sapply(threshold) %>% str()
+
+map_dbl(mtcars,mean)
+
+nycflights13::flights  %>% map(class)
+
+iris %>% map(unique) %>% map_int(length)
+
+u = c(-10,0,10,100)
+
+m = u %>% map(rnorm, n=10)
+
+output = vector("list", length(u) )
+
+for (i in seq_along(u)){
+  output[[i]] = rnorm(n,u[i])
+}
+
+mtcars %>% map_lgl(is.factor)
+
+map(-2:2, rnorm, n = 5)
+map_dbl(-2:2, rnorm, n = 5)
+
+
+map(~. lm(mpg ~ wt, data = .))
+
+
+# get the safely function use to debug code with map use
+safe_log <- safely(log)
+ str(safe_log(10))
+
+ x <- list(1, 10, "a")
+ y <- x %>% map(safely(log))
+ str(y)
+ 
+ #get the transpose form of safely function to make the flagging of corrections
+ 
+y = y %>% transpose() 
+str(y)
+
+is_ok = y$error %>% map_lgl(is_null)
+x[!is_ok]
+
+y$result[is_ok] %>% flatten_dbl()
